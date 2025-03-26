9c520be433e6b06d065b8a7e4a949dc8f06fa88a
atrox3d <atrox3d@gmail.com>
2025-03-26 14:41:11 +0100

    manage lifespan message

--- handlers.py
+++ handlers.py
@@ -2,8 +2,17 @@ from custom_types import Receive, Scope, Send
 from helpers import logger
 
 async def handle_lifespan(scope: Scope, receive: Receive, send: Send) -> None:
-    pass
-
+    assert scope["type"] == "lifespan"
+    while True:
+        message = await receive()
+        logger.info(f'got message: {message}')
+        
+        if message["type"] == "lifespan.startup":
+            await send({"type": "lifespan.startup.complete"})
+        elif message["type"] == "lifespan.shutdown":
+            await send({"type": "lifespan.shutdown.complete"})
+            break
+    
 
 async def handle_http(scope: Scope, receive: Receive, send: Send) -> None:
     pass
